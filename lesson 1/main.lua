31d20402e429ab2789cb8242d91437014c582642
daskie911 <imevil2302@gmail.com>
2024-06-16 15:06:06 +0200

    lesson 1

---
+++ lesson 1/main.lua
@@ -0,0 +1,13 @@
+print("Hello world!") -- This is a string
+print(1) -- This is integer
+print(1 + 3) -- This is an expression
+
+-- boolean
+
+print(true)
+print(false)
+
+-- if statement
+if 1 + 1 == 2 then
+    print("Math is working")
+end
