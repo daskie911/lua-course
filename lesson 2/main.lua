5b473b2805f50823a15d832ff16936a75ecb9288
daskie911 <imevil2302@gmail.com>
2024-06-19 22:11:01 +0200

    learner variables

---
+++ lesson 2/main.lua
@@ -0,0 +1,8 @@
+local VariableName = "Hello World" -- This is a variable with a string value
+
+if 1 + 1 == 2 then
+    local secondVariable = "Math is working" -- This is a local variable with a string value
+    print(secondVariable) -- This will print "Math is working"
+end
+
+print(VariableName) -- This will print "Hello World"
